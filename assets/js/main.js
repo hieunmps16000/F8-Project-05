94083bc1e91345126339425ee13ea36b7d21ca58
Nguyen Minh Hieu <lotous12a4@gmail.com>
2025-02-11 21:35:53 +0700

    feat(menu-mobile): add active navbar link

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,8 @@ const $$ = document.querySelectorAll.bind(document);
 const menuToggle = $('.menu-toggle');
 const headerMobile = $('.header-mobile');
 const menuOverlay = $('.menu__overlay');
+const menuPc = $('#menu-pc');
+const menuMobile = $('#menu-mobile');
 
 menuToggle.onclick = () => {
     headerMobile.classList.add('header-mobile--active');
@@ -13,3 +15,15 @@ menuOverlay.onclick = () => {
     headerMobile.classList.remove('header-mobile--active');
 }
 
+menuMobile.innerHTML = menuPc.innerHTML;
+
+const navbarLinks = $$('.navbar__link');
+
+navbarLinks.forEach(navbarLink => {
+    navbarLink.onclick = (e) => {
+        $$('.navbar__link--active').forEach(navbarLinkActive => {
+            navbarLinkActive.classList.remove('navbar__link--active');
+        })
+        e.target.classList.add('navbar__link--active');
+    }
+});
